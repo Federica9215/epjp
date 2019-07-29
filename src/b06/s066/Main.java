b587b05d341a492f643bb893d1e6d61aa91d458f
Federica Cosenza <federica.cosenza92@gmail.com>
2019-07-29 10:38:29 +0200

    Aggiunte di //

--- src/b06/s066/Main.java
+++ src/b06/s066/Main.java
@@ -1,8 +1,9 @@
+
 package b06.s066;
 
 public class Main {
     public static void main(String[] args) {
-        Barker[] barkers = new Barker[2];
+        Barker[] barkers = new Barker[2]; //barkers=reference che si riferisce all'interfaccia
         
         barkers[0] = new Fox();
         barkers[1] = new Dog();
@@ -11,10 +12,10 @@ public class Main {
             System.out.println(barker.bark());
         }
         
-        BarkAndWag baw = new Dog();
+        BarkAndWag baw = new Dog(); //baw=reference che si riferisce all'interfaccia BarkAndWag
         System.out.println(baw.tailWaggingSpeed());
 
-        Dog dog = new Dog();
+        Dog dog = new Dog(); //dog=reference che si riferisce all'oggetto Dog
         System.out.println(dog.bark(3));
     }
 }
