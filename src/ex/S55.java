c699eb8bfa448d7e5441410fc55849c210607ace
Federica Cosenza <federica.cosenza92@gmail.com>
2019-07-30 15:19:05 +0200

    Cambiamenti

--- src/ex/S55.java
+++ src/ex/S55.java
@@ -1,35 +1,38 @@
 package ex;
 
 public class S55 {
-	private static long fibonacci;
-
-
-
 	/**
 	 * Add up all the numbers in the passed closed interval
 	 * 
 	 * @param first the left limit
 	 * @param last  the right limit
 	 * @return the sum of all the numbers, or zero
+	 * @throws Exception
 	 */
-
-	public static long sum(int first, int last)
-
-	{
+	public static long sum(int first, int last) {
 		long sum = 0L;
 		if (last < first) {
 			return 0L;
 		} else {
-
 			for (int i = first; i <= last; i++)
 				sum += i;
-
 		}
 
 		return sum;
+	}
 
-		// TODO
+	public static long factorialEx(int value) throws Exception {
+		if (value < 0) {
+			throw new Exception("Non si può fare il fattoriale di un numero negativo");
+		}
+		return factorial(value);
+	}
 
+	public static long sumEx(int first, int last) throws Exception {
+		if (last < first) {
+			throw new Exception("errore");
+		}
+		return sum(first, last);
 	}
 
 	/**
@@ -42,7 +45,7 @@ public class S55 {
 	public static long evenSum(int first, int last) {
 		long evenSum = 0L;
 		int i = 0;
-		for (i = first; i <=last;) {
+		for (i = first; i <= last;) {
 			if (i % 2 == 0) {
 				return evenSum += i;
 
@@ -64,30 +67,30 @@ public class S55 {
 	 */
 	public static long factorial(int value) {
 		long factorial = 0L;
-		int i=0;
-		if(value==0) {
-			
-			return factorial=1L; 
-			
+		int i = 0;
+		if (value == 0) {
+
+			return factorial = 1L;
+
 		}
-		
-		if(value<0) {
-			
-			return factorial=0L; 
-			
+
+		if (value < 0) {
+
+			return factorial = 0L;
+
 		}
-		
-		if(value>0) {
-			
-			for (i=value; i==1; i--) {
-				i*=(i-1)*(i-2); 
+
+		if (value > 0) {
+
+			for (i = value; i == 1; i--) {
+				i *= (i - 1) * (i - 2);
 			}
-			return factorial=i*(i-1)*(i-2);
+			return factorial = i * (i - 1) * (i - 2);
 		}
-			
-			return factorial;
+
+		return factorial;
 		// TODO
-		
+
 	}
 
 	/**
@@ -97,22 +100,19 @@ public class S55 {
 	 * @return the Fibonacci number of value, or zero
 	 */
 	public static long fibonacci(int value) {
-		if (value==0) {
-			return value; 
+		if (value == 0) {
+			return value;
 		}
-		if (value <=1) {
-			return value; 
+		if (value <= 1) {
+			return value;
 		} else if (value > 1 && value < 20) {
-			
-			return fibonacci(value-1)+fibonacci(value-2);
+
+			return fibonacci(value - 1) + fibonacci(value - 2);
 		}
 		return 0;
-		
-		
+
 	}
 
-	
-	
 	/**
 	 * Multiplication table
 	 * 
@@ -121,26 +121,17 @@ public class S55 {
 	 */
 	public static int[][] multiplicationTable(int value) {
 		int[][] result = new int[value][value];
-		int i=0; 
-		int j=0;
-		
-		for (i = 1; i<=value; i++) {
-			for (j=1; j<value; j++) {
-				result[i][j]=(i+1)*(j+1);
+		int i = 0;
+		int j = 0;
+
+		for (i = 1; i <= value; i++) {
+			for (j = 1; j < value; j++) {
+				result[i][j] = (i + 1) * (j + 1);
 			}
 		}
-		
 
 		// TODO
 
 		return result;
 	}
-
-	private static int[] result(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
-
-
-		
--- test/ex/S55Test.java
+++ test/ex/S55Test.java
@@ -8,87 +8,111 @@ import org.junit.jupiter.api.Test;
 
 class S55Test {
 
-    @Test
-    void sumPositive() {
-        long actual = S55.sum(1, 3);
+	@Test
+	void sumPositive() {
+		long actual = S55.sum(1, 3);
 
-        assertThat(actual, is(6L));
-    }
+		assertThat(actual, is(6L));
+	}
 
-    @Test
-    void sumNegPos() {
-        long actual = S55.sum(-3, 3);
+	@Test
+	void sumExSimple() {
+		try {
+			S55.sumEx(3, 1);
+			fail("Shouldn't get to this point");
+		} catch (Exception e) {
+			String expected = "errore";
+			assertThat(e.getMessage(), is(expected));
+		}
+	}
 
-        assertThat(actual, is(0L));
-    }
+	@Test
+	void sumNegPos() {
+		long actual = S55.sum(-3, 3);
 
-    @Test
-    void sumSingle() {
-        long actual = S55.sum(1003, 1003);
+		assertThat(actual, is(0L));
+	}
 
-        assertThat(actual, is(1003L));
-    }
+	@Test
+	void sumSingle() {
+		long actual = S55.sum(1003, 1003);
 
-    @Test
-    void sumEmpty() {
-        long actual = S55.sum(1003, 1002);
+		assertThat(actual, is(1003L));
+	}
 
-        assertThat(actual, is(0L));
-    }
+	@Test
+	void sumEmpty() {
+		long actual = S55.sum(1003, 1002);
 
-    @Test
-    void evenSumPositive() {
-        long actual = S55.evenSum(1, 3);
+		assertThat(actual, is(0L));
+	}
 
-        assertThat(actual, is(2L));
-    }
+	@Test
+	void evenSumPositive() {
+		long actual = S55.evenSum(1, 3);
 
-    @Test
-    void evenSumNegPos() {
-        long actual = S55.evenSum(-3, 3);
+		assertThat(actual, is(2L));
+	}
 
-        assertThat(actual, is(0L));
-    }
+	@Test
+	void evenSumNegPos() {
+		long actual = S55.evenSum(-3, 3);
 
-    @Test
-    void evenSumSingle() {
-        long actual = S55.evenSum(1002, 1002);
+		assertThat(actual, is(0L));
+	}
 
-        assertThat(actual, is(1002L));
-    }
+	@Test
+	void evenSumSingle() {
+		long actual = S55.evenSum(1002, 1002);
 
-    @Test
-    void evenSumEmpty() {
-        long actual = S55.evenSum(1003, 1002);
+		assertThat(actual, is(1002L));
+	}
 
-        assertThat(actual, is(0L));
-    }
+	@Test
+	void evenSumEmpty() {
+		long actual = S55.evenSum(1003, 1002);
 
-    @Test
-    void factorialFour() {
-        long actual = S55.factorial(4);
-        
-        assertThat(actual, is(24L));
-    }
+		assertThat(actual, is(0L));
+	}
 
-    @Test
-    void factorialNegative() {
-        long actual = S55.factorial(-4);
-        
-        assertThat(actual, is(0L));
-    }
+	@Test
+	void factorialFour() {
+		long actual = S55.factorial(4);
 
-    @Test
-    void fibonacci() {
-    	int actual = 8;
-    	
-    	assertThat(actual, is(8));
-    }
+		assertThat(actual, is(24L));
+	}
 
-    @Test
-    void multiplicationTable() {
-    	int actual = 8;
-    	
-    	assertThat(actual, is(8));
-    }
+	
+
+	@Test
+	void factorialExSimple() {
+		try {
+			S55.factorialEx(-1);
+
+		} catch (Exception e) {
+			String expected = "Non si può fare il fattoriale di un numero negativo";
+			assertThat(e.getMessage(), is(expected));
+		}
+	}
+
+	@Test
+	void factorialNegative() {
+		long actual = S55.factorial(-4);
+
+		assertThat(actual, is(0L));
+	}
+
+	@Test
+	void fibonacci() {
+		int actual = 8;
+
+		assertThat(actual, is(8));
+	}
+
+	@Test
+	void multiplicationTable() {
+		int actual = 8;
+
+		assertThat(actual, is(8));
+	}
 }
