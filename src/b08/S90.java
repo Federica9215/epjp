463113a3fb218c513c4e6c8602f6df25b714dd2b
Federica Cosenza <federica.cosenza92@gmail.com>
2019-07-31 10:15:04 +0200

    Cambiamenti

---
+++ src/b08/S90.java
@@ -0,0 +1,40 @@
+package b08;
+
+import java.util.Scanner;
+
+public class S90 {
+	private static int i;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("How much number?");
+
+		int n = 0;
+		n=scanner.nextInt();
+
+		 // LOOP to get number size
+
+			System.out.println("Your number are: " + n);
+	
+
+		int numbers[] = new int[n];
+		double average = 0;
+		double sum = 0;
+
+		
+		while (scanner.hasNext()) { // finchè nello scanner c'è qlks da leggere
+			if (scanner.hasNextDouble()) { // se nelLo scanner c e qlks da leggere
+				for (int i =0; i < n; i++)
+					numbers[i] = scanner.nextInt();
+				
+				average = (sum + numbers[i])/ i;
+				scanner.nextDouble(); // allora dammi il risultato
+			} else {
+				System.out.println(" " + scanner.next());
+			}
+		}
+		scanner.close(); // see try-with-resources
+		System.out.println("The average is" + average);
+	}
+}
