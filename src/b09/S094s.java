d84fbf27706d9578aca0b5ebb259eda61bd9eec2
Federica Cosenza <federica.cosenza92@gmail.com>
2019-08-01 11:32:52 +0200

    Cambiamenti

--- src/b09/S094s.java
+++ src/b09/S094s.java
@@ -8,10 +8,31 @@ public class S094s {
 	
 
 
-	public static ArrayList<Integer> aJoin(ArrayList<Integer> a, ArrayList<Integer> b) {
+	public static ArrayList<Integer> join(int[] a, int [] b) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
-		result.addAll(a);
-		result.addAll(b);
+		int i=0;
+		int j=0;
+		
+		for (;i<=a.length; i++) {
+			if(a[i]<b[j]) {
+				result.add(a[i]);
+			}
+			else if (a[i]>b[j]) {
+				result.add(b[j]);
+				j++;
+				i--;
+			
+			}
+			else if (i==a.length) {
+				for(;j<b.length;j++) {
+					result.add(b[j]);
+				}
+			}else if (j==b.length) {
+				for(;i<a.length;i++) {
+					result.add(a[i]);
+				}
+		}
+		}
 		return result;
 
 	}
---
+++ src/b09/S094sTest.java
@@ -0,0 +1,22 @@
+package b09;
+
+
+
+
+import static org.junit.Assert.assertArrayEquals;
+
+
+
+import org.junit.jupiter.api.Test;
+
+class S094sTest {
+
+	@Test
+	void test() {
+		int[] a = {3,7,12};
+		int[] b = {1,5,6};
+		
+		assertArrayEquals(a,b);
+	}
+
+}
