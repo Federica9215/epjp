73bf985261214fd10ee26ec87996bd18c6ba89d8
Federica Cosenza <federica.cosenza92@gmail.com>
2019-08-01 18:01:10 +0200

    New

--- src/b11/Main.java
+++ src/b11/Main.java
@@ -1,8 +1,20 @@
 package b11;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
+		ArrayList<Person> persons = new ArrayList<Person>();
+		persons.add(new Villain ("Tiziana", 15, 4));
+		persons.add(new Villain ("Gino", 7, 3));
+		persons.add(new Hero ("Giulio", 10, 5));
+		persons.add(new Hero ("Federica", 8, 8));
+		persons.add(new CommonPerson ("Silvia", 20, 2));
+		persons.add(new CommonPerson ("Vito", 5, 10));
+		
+		ArrayList<Person> prisoners = new ArrayList<Person>();
+		ArrayList<Person> dead = new ArrayList<Person>();
 		
 
 	}
